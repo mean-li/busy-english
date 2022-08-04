fea56f80ac774648b73c31a7bc43d1eb47985214
liviacs <liviacaroline193@gmail.com>
2022-08-04 16:40:57 -0300

    Add section 'type2'

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import About from './about'
 import Home from './home'
+import Type2 from './type2'
 
 function LandingPage() {
   return (
     <>
       <Home />
       <About />
+      <Type2 />
     </>
   )
 }
