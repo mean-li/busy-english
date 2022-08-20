2e8516dc7b76d5f5cd8ab23eec8373c4dea55969
Emily May <targa.pe@gmail.com>
2022-08-20 14:10:30 -0300

    Fixed Classes Card Title padding-top on Mobile Screens

--- src/pages/classes.jsx
+++ src/pages/classes.jsx
@@ -52,7 +52,7 @@ function Classes() {
               <div className="card rounded-card p-5 d-flex flex-column">
                 <div className="card-body gap-3 d-flex justify-content-start flex-column">
                   <div className="d-flex align-items-center justify-content-center">
-                    <h1 className="card-title be-text about-title text text-center position-absolute">
+                    <h1 className="card-title be-text about-title text text-center">
                       Regular Conversation
                     </h1>
                   </div>
@@ -80,7 +80,7 @@ function Classes() {
               <div className="card rounded-card p-5 d-flex flex-column">
                 <div className="card-body gap-3 d-flex justify-content-start flex-column">
                   <div className="d-flex align-items-center justify-content-center">
-                    <h1 className="card-title about-title text text-center position-absolute">
+                    <h1 className="card-title about-title text text-center">
                       Business English
                     </h1>
                   </div>
@@ -107,7 +107,7 @@ function Classes() {
               <div className="card rounded-card p-5 d-flex flex-column">
                 <div className="card-body gap-3 d-flex justify-content-start flex-column">
                   <div className="d-flex align-items-center justify-content-center">
-                    <h1 className="card-title about-title text text-center position-absolute">
+                    <h1 className="card-title about-title text text-center">
                       Travel English
                     </h1>
                   </div>
@@ -134,7 +134,7 @@ function Classes() {
               <div className="card rounded-card p-5 d-flex flex-column">
                 <div className="card-body gap-3 d-flex justify-content-start flex-column">
                   <div className="d-flex align-items-center justify-content-center">
-                    <h1 className="card-title about-title text text-center position-absolute">
+                    <h1 className="card-title about-title text text-center">
                       Proficiency Test
                     </h1>
                   </div>
