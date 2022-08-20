8dc0ce13d3c8a3107dc23eb34962870f419bd6c7
Emily May <targa.pe@gmail.com>
2022-08-20 16:23:09 -0300

    Classes section 100vh

--- src/components/CallButton.jsx
+++ src/components/CallButton.jsx
@@ -4,7 +4,9 @@ function CallButton() {
   return (
     <button className="custom-button-1 btn btn-secondary">
       <PopupButton
-        url="https://calendly.com/nicoletongu"
+        styles={{
+          overflow: 'hidden',
+        }} url="https://calendly.com/nicoletongu"
         /*
          * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
          * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
@@ -12,6 +14,7 @@ function CallButton() {
         rootElement={document.getElementById('root')}
         className="w-100 bg-transparent border-0 button-link button-1-text"
         text="Agende um horário comigo"
+
       />
     </button>
   )
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,49 +5,50 @@ function About() {
     <>
       <section
         id="about"
-        className="bg-light rounded-container caixa-responsiva text-dark p-5 d-flex"
       >
-        {/* a div abaixo é da caixa sem background */}
-        <div className="container container-responsivo d-flex align-items-center justify-content-center flex-column">
-          {/* a div abaixo é dos textos */}
-          <div className="d-flex flex-column">
-            <div className="d-flex justify-content-start flex-column">
-              <h1 className="about-title text text-start lh-sm">
-                Oii, meu nome é Nicole :)
-              </h1>
-              <h2 className="about-subtitle text text-start lh-sm">
-                Meu histórico
-              </h2>
-              <h3 className="about-text text text-start lh-sm">
-                Eu sou uma estudante de economia que adora ensinar e aprender
-                idiomas! Eu estudo inglês desde os 9 anos de idade, foi paixão à
-                primeira vista! Fiz o First Cambridge Certificate - um teste
-                internacional que nivela seu inglês - e meu nível de conversação
-                era C1 (fluente); é por isso que gosto de ensinar habilidades de
-                conversação!
-              </h3>
+        <div className="bg-light rounded-container caixa-responsiva text-dark p-5 d-flex">
+          {/* a div abaixo é da caixa sem background */}
+          <div className="container container-responsivo d-flex align-items-center justify-content-center flex-column">
+            {/* a div abaixo é dos textos */}
+            <div className="d-flex flex-column">
+              <div className="d-flex justify-content-start flex-column">
+                <h1 className="about-title text text-start lh-sm">
+                  Oii, meu nome é Nicole :)
+                </h1>
+                <h2 className="about-subtitle text text-start lh-sm">
+                  Meu histórico
+                </h2>
+                <h3 className="about-text text text-start lh-sm">
+                  Eu sou uma estudante de economia que adora ensinar e aprender
+                  idiomas! Eu estudo inglês desde os 9 anos de idade, foi paixão à
+                  primeira vista! Fiz o First Cambridge Certificate - um teste
+                  internacional que nivela seu inglês - e meu nível de conversação
+                  era C1 (fluente); é por isso que gosto de ensinar habilidades de
+                  conversação!
+                </h3>
+              </div>
             </div>
-          </div>
-          {/* a div abaixo é da imagem */}
-          <div className="d-flex flex-column">
-            <div className="d-flex justify-content-start flex-column">
-              <h1 className="about-subtitle text text-start lh-sm">
-                O que eu faço
-              </h1>
-              <h2 className="about-text text text-start fw-normal lh-sm">
-                Ao longo da minha carreira como professora notei que a maior
-                parte dos alunos teve a parte da conversação negligenciada;
-                aprender a gramática de uma língua é realmente importante, mas
-                na vida real se você aprende uma língua mas não consegue
-                falar... então para que isso foi útil?
-              </h2>
-              <h2 className="about-text text text-start fw-normal lh-sm">
-                Por isso desenvolvi um material específico para praticar a
-                conversação enquanto fala sobre seus interesses.
-              </h2>
-              <h2 className="about-text text text-start fw-normal lh-sm">
-                Eu ofereço diversos tipos de aulas:
-              </h2>
+            {/* a div abaixo é da imagem */}
+            <div className="d-flex flex-column">
+              <div className="d-flex justify-content-start flex-column">
+                <h1 className="about-subtitle text text-start lh-sm">
+                  O que eu faço
+                </h1>
+                <h2 className="about-text text text-start fw-normal lh-sm">
+                  Ao longo da minha carreira como professora notei que a maior
+                  parte dos alunos teve a parte da conversação negligenciada;
+                  aprender a gramática de uma língua é realmente importante, mas
+                  na vida real se você aprende uma língua mas não consegue
+                  falar... então para que isso foi útil?
+                </h2>
+                <h2 className="about-text text text-start fw-normal lh-sm">
+                  Por isso desenvolvi um material específico para praticar a
+                  conversação enquanto fala sobre seus interesses.
+                </h2>
+                <h2 className="about-text text text-start fw-normal lh-sm">
+                  Eu ofereço diversos tipos de aulas:
+                </h2>
+              </div>
             </div>
           </div>
         </div>
