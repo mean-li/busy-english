69c84a225e15e9ef2361c0264d2cef09c39fdf01
Andressa Varela <andressaavarela@gmail.com>
2022-08-24 14:58:04 -0300

    Fix home image's border radius on medium screens

--- src/components/CallButton.jsx
+++ src/components/CallButton.jsx
@@ -5,8 +5,9 @@ function CallButton() {
     <button className="custom-button-1 btn btn-secondary">
       <PopupButton
         styles={{
-          overflow: 'hidden',
-        }} url="https://calendly.com/nicoletongu"
+          overflow: 'hidden'
+        }}
+        url="https://calendly.com/nicoletongu"
         /*
          * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
          * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
@@ -14,7 +15,6 @@ function CallButton() {
         rootElement={document.getElementById('root')}
         className="w-100 bg-transparent border-0 button-link button-1-text"
         text="Agende um horário comigo"
-
       />
     </button>
   )
